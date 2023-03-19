59252725d37bd833d02138164a63791ac2b3c22a
Manuel Parra <th3nolo@gmail.com>
2023-03-19 15:08:44 -0400

    Docker file update

--- .devcontainer/start-android-studio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/start-vnc.sh &
-
-# Replace the path with the actual path to the Android Studio executable
-/opt/android-studio/bin/studio.sh
--- .devcontainer/start-vnc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-Xvfb :1 -screen 0 1280x720x24+32 &
-export DISPLAY=:1
-
-x11vnc -forever -shared -rfbport 5901 -display :1 &
-
-cd /opt && ./utils/launch.sh --listen 6080 --vnc localhost:5901
